3042edc4c211a32dd4e1de65e7285243d164a2eb
Guillem Gari <ggari@robotnik.es>
2023-05-31 19:24:01 +0200

    removed unused entrypoint

--- docker/ros_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment
-source "/opt/ros/humble/setup.bash"
-source "/home/ros/ros2_ws/install/setup.bash"
-#source "/opt/ros/noetic/setup.bash"
-#source "/home/ros/catkin_ws/devel/setup.bash"
-exec "$@"
