63bd8150a403aae4b1145b3c4a3cd93d99eeff7a
EmimaJiva <emji@ai2.upv.es>
2023-06-23 13:20:55 +0200

    Add delivery mission

--- fleet_adapter_mqtt/fleet_adapter_mqtt/fleet_adapter_mqtt/RobotDelivery.py
+++ fleet_adapter_mqtt/fleet_adapter_mqtt/fleet_adapter_mqtt/RobotDelivery.py
@@ -78,54 +78,6 @@ class Dispenser(Node):
         else: 
             self.status[msg.request_guid] =  DISPENSER_RESULT_ACKNOWLEDGED
             self.get_logger().info('Aknowledged')
-
-        # if msg.target_guid != self.name:
-        #     return self.get_logger().info('Different name')
-        # self.get_logger().info('REQUEST RECEIVED')
-        # self.tasks[msg.request_guid] = True
-        # self.status [msg.request_guid]
-        # if self.prevstatus.get[msg.request_guid] is not None and \
-        #     self.prevstatus[msg.request_guid] != self.status[msg.request_guid]:
-        #     self.get_logger().info('Hacer lo que queramos')
-        #     self.prevstatus[msg.request_guid] = self.status[msg.request_guid]
-        # else:
-        #     self.prevstatus[msg.request_guid] = self.status[msg.request_guid]
-
-        # if self.tasks.get(msg.request_guid):
-        #     status = DISPENSER_RESULT_SUCCESS
-        #     self.get_logger().info('Status "%d"' % status)
-        # else:
-        #     self.tasks[msg.request_guid] = False
-        #     status = DISPENSER_RESULT_ACKNOWLEDGED
-        #     self.get_logger().info('Status "%d"' % status)
-        #     self.timer = self.create_timer(
-        #         1,
-        #         partial(self._timer_cb, msg=msg)
-        #     )
-        # result = dispenser_msgs.DispenserResult()
-        # result.time = self.get_clock().now().to_msg()
-        # result.status = status
-        # result.source_guid = self.name
-        # result.request_guid = msg.request_guid
-
-        # self.get_logger().info('Publish "%d"' % result.status)
-        # self.result_pub.publish(result)
-
-    def _timer_cb(self, msg):
-        if not self.timer:
-            return
-
-        result = dispenser_msgs.DispenserResult()
-        result.time = self.get_clock().now().to_msg()
-        result.status = DISPENSER_RESULT_SUCCESS
-        result.source_guid = self.name
-        result.request_guid = msg.request_guid
-
-        self.get_logger().info('Publish timer "%d"' % result.status)
-        self.result_pub.publish(result)
-        self.success_flag = True
-        self.tasks[msg.request_guid] = True
-        self.timer.cancel()
     
     def wait_robot(self):
         self.get_logger().info('Hacemos algo')
