fd63f9369475f859e95e9b040acbccf9863979bf
Guillem Gari <ggari@robotnik.es>
2024-08-01 14:19:10 +0200

    Corrected to avoid peps

--- robotnik_rmf_fleet_adapter_mqtt/robotnik_rmf_fleet_adapter_mqtt/RobotClientAPI.py
+++ robotnik_rmf_fleet_adapter_mqtt/robotnik_rmf_fleet_adapter_mqtt/RobotClientAPI.py
@@ -14,7 +14,7 @@
 
 # RobotClientAPI.py
 #
-# @author Emima Jiva. (c) ai2-UPV Todos los derechos reservados.
+# @author Emima Jiva. (c) ai2-UPV
 #
 # Proyecto "WASHCARROB"
 # Version: 1.0
@@ -26,9 +26,11 @@ import math
 import numpy as np
 import time
 
+
 class MaxRetriesExceededError(Exception):
     """Raised when the maximum number of connection retries is exceeded."""
     pass
+
 
 def quaternion_from_euler(roll, pitch, yaw):
     """
@@ -47,6 +49,7 @@ def quaternion_from_euler(roll, pitch, yaw):
     qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
     qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
     return [qx, qy, qz, qw]
+
 
 def euler_from_quaternion(x, y, z, w):
     """
@@ -67,8 +70,9 @@ def euler_from_quaternion(x, y, z, w):
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
     yaw_z = math.atan2(t3, t4)
+    # in radians
+    return roll_x, pitch_y, yaw_z
 
-    return roll_x, pitch_y, yaw_z # in radians
 
 '''
     The RobotAPI class is a wrapper for API calls to the robot. Here users
@@ -78,53 +82,94 @@ def euler_from_quaternion(x, y, z, w):
     these functions.
 '''
 
+
 class RobotAPI:
     # The constructor below accepts parameters typically required to submit
     # http requests. Users should modify the constructor as per the
     # requirements of their robot's API
-    def __init__(self, broker: str, port: int, keep_alive: int, anonymous_access: bool, user: str, password: str, \
-                pose_topic: str, feedback_topic: str, result_topic: str, battery_topic: str, \
-                goal_dist: int, dispenser_topic: str, ingestor_topic: str, start_ae_topic: str, door_request_topic: str, \
-                dock_request_topic: str, undock_request_topic: str):
-        #Delivery topic
+    def __init__(
+        self,
+        broker: str,
+        port: int,
+        keep_alive: int,
+        anonymous_access: bool,
+        user: str,
+        password: str,
+        pose_topic: str,
+        feedback_topic: str,
+        result_topic: str,
+        battery_topic: str,
+        goal_dist: int,
+        dispenser_topic: str,
+        ingestor_topic: str,
+        start_ae_topic: str,
+        door_request_topic: str,
+        dock_request_topic: str,
+        undock_request_topic: str
+    ):
+        # Delivery topic
         self.dispenser_topic = dispenser_topic
         self.ingestor_topic = ingestor_topic
         self.start_ae_topic = start_ae_topic
         self.door_request_topic = door_request_topic
         self.dock_request_topic = dock_request_topic
         self.undock_request_topic = undock_request_topic
-        #Distance parameter
+        # Distance parameter
         self.goal_dist = goal_dist
-        #Position information
+        # Position information
         self.x = {}
         self.y = {}
         self.theta = {}
         self.x_goal = {}
         self.y_goal = {}
         self.theta_goal = {}
-        #Dictionary robot-pose
+        # Dictionary robot-pose
         self.robotpose = {}
-        #Dictionary battery level
+        # Dictionary battery level
         self.battery = {}
-        #Dictionary result
+        # Dictionary result
         self.resultgoal = {}
-        #Dictionary result
+        # Dictionary result
         self.feedback = {}
-        #A dict with existing robots
+        # A dict with existing robots
         self.robots = {}
-        #MQTT Connection
-        self.client = self.connect_mqtt(broker, port, keep_alive, anonymous_access, user, password, \
-                                        pose_topic, feedback_topic, result_topic, battery_topic)
+        # MQTT Connection
+        self.client = self.connect_mqtt(
+            broker,
+            port,
+            keep_alive,
+            anonymous_access,
+            user,
+            password,
+            pose_topic,
+            feedback_topic,
+            result_topic,
+            battery_topic
+        )
         self.client.loop_start()
 
-    def connect_mqtt(self, broker, port, keep_alive, anonymous_access, user, password, \
-                     pose_topic, feedback_topic, result_topic, battery_topic):
+    def connect_mqtt(
+        self,
+        broker,
+        port,
+        keep_alive,
+        anonymous_access,
+        user,
+        password,
+        pose_topic,
+        feedback_topic,
+        result_topic,
+        battery_topic
+    ):
         def on_connect(client, userdate, flags, rc):
             if rc == 0:
                 print("Connected to MQTT Broker")
             else:
                 print("Failed to connect")
-        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1,'fleet-adapter')
+        client = mqtt.Client(
+            mqtt.CallbackAPIVersion.VERSION1,
+            'fleet-adapter'
+        )
         client.on_connect = on_connect
         client.message_callback_add(pose_topic, self.on_message_pose)
         client.message_callback_add(result_topic, self.on_message_result)
@@ -159,27 +204,27 @@ class RobotAPI:
         return client
 
     def on_message_pose(self, client, userdata, msg):
-        decoded_message=str(msg.payload.decode("utf-8"))
-        pose=json.loads(decoded_message)['pose']['pose']
+        decoded_message = str(msg.payload.decode("utf-8"))
+        pose = json.loads(decoded_message)['pose']['pose']
         robot = msg.topic.split("/")[1]
         self.robotpose[robot] = pose
         self.robots[robot] = robot
 
     def on_message_result(self, client, userdata, msg):
-        decoded_message=str(msg.payload.decode("utf-8"))
-        result=json.loads(decoded_message)['status']['status']
+        decoded_message = str(msg.payload.decode("utf-8"))
+        result = json.loads(decoded_message)['status']['status']
         robot = msg.topic.split("/")[1]
         self.resultgoal[robot] = result
 
     def on_message_feedback(self, client, userdata, msg):
-        decoded_message=str(msg.payload.decode("utf-8"))
-        feed=json.loads(decoded_message)['status']['status']
+        decoded_message = str(msg.payload.decode("utf-8"))
+        feed = json.loads(decoded_message)['status']['status']
         robot = msg.topic.split("/")[1]
         self.feedback[robot] = feed
 
     def on_message_battery(self, client, userdata, msg):
-        decoded_message=str(msg.payload.decode("utf-8"))
-        bat=json.loads(decoded_message)['level']
+        decoded_message = str(msg.payload.decode("utf-8"))
+        bat = json.loads(decoded_message)['level']
         robot = msg.topic.split("/")[1]
         self.battery[robot] = bat
 
@@ -187,45 +232,73 @@ class RobotAPI:
         ''' Return [x, y, theta] expressed in the robot's coordinate frame or
             None if any errors are encountered'''
         if self.robots.get(robot_name) is not None:
-            self.x[robot_name] = round(self.robotpose[robot_name]['position']['x'],2)
-            self.y[robot_name] = round(self.robotpose[robot_name]['position']['y'],2)
-            self.theta[robot_name] = round(euler_from_quaternion(
-            self.robotpose[robot_name]['orientation']['x'], self.robotpose[robot_name]['orientation']['y'],
-            self.robotpose[robot_name]['orientation']['z'], self.robotpose[robot_name]['orientation']['w'])[2],2)
-            return [self.x[robot_name],self.y[robot_name],self.theta[robot_name]]
+            self.x[robot_name] = round(
+                self.robotpose[robot_name]['position']['x'],
+                2
+            )
+            self.y[robot_name] = round(
+                self.robotpose[robot_name]['position']['y'],
+                2
+            )
+            self.theta[robot_name] = round(
+                euler_from_quaternion(
+                    self.robotpose[robot_name]['orientation']['x'],
+                    self.robotpose[robot_name]['orientation']['y'],
+                    self.robotpose[robot_name]['orientation']['z'],
+                    self.robotpose[robot_name]['orientation']['w']
+                )[2],
+                2
+            )
+            return [
+                self.x[robot_name],
+                self.y[robot_name],
+                self.theta[robot_name]
+            ]
         else:
             print("No position for " + robot_name)
             return None
 
-
-    def navigate(self,
-                 robot_name: str,
-                 cmd_id: int,
-                 pose,
-                 map_name: str):
+    def navigate(
+        self,
+        robot_name: str,
+        cmd_id: int,
+        pose,
+        map_name: str
+    ):
         ''' Request the robot to navigate to pose:[x,y,theta] where x, y and
             and theta are in the robot's coordinate convention. This function
             should return True if the robot has accepted the request,
             else False'''
-        self.x_goal[robot_name] = round(pose[0],2)
-        self.y_goal[robot_name] = round(pose[1],2)
-        self.theta_goal[robot_name] = round(pose[2],2)
-        orientation = quaternion_from_euler(0,0,round(pose[2],2))
-        data = {"goal":{"target_pose":{"header":{
-                                            "frame_id":map_name},
-                                    "pose":{
-                                            "position":{
-                                                "x":self.x_goal[robot_name],
-                                                "y":self.y_goal[robot_name],
-                                                "z":0
-                                            },
-                                            "orientation":{
-                                                "x":orientation[0],
-                                                "y":orientation[1],
-                                                "z":orientation[2],
-                                                "w":orientation[3]
-                                            }}}}}
-        self.client.publish("goal/"+robot_name ,json.dumps(data), 2)
+        self.x_goal[robot_name] = round(pose[0], 2)
+        self.y_goal[robot_name] = round(pose[1], 2)
+        self.theta_goal[robot_name] = round(pose[2], 2)
+        orientation = quaternion_from_euler(0, 0, round(pose[2], 2))
+        data = {
+            "goal": {
+                "target_pose": {
+                    "header": {
+                        "frame_id": map_name
+                    },
+                    "pose": {
+                        "position": {
+                            "x": self.x_goal[robot_name],
+                            "y": self.y_goal[robot_name],
+                            "z": 0
+                        },
+                        "orientation": {
+                            "x": orientation[0],
+                            "y": orientation[1],
+                            "z": orientation[2],
+                            "w": orientation[3]
+                        }
+                    }
+                }
+            }
+        }
+        self.client.publish(
+            "goal/" + robot_name, json.dumps(data),
+            2
+        )
         if self.feedback.get(robot_name) is not None and self.feedback[robot_name] == 1:
             self.feedback[robot_name] = 0
             return True
@@ -237,7 +310,10 @@ class RobotAPI:
                       cmd_id: int,
                       process: str,
                       map_name: str):
-        print(robot_name + " " + str(cmd_id) + " " + process + " " + map_name, flush=True)
+        print(
+            robot_name + " " + str(cmd_id) + " " + process + " " + map_name,
+            flush=True
+        )
         return True
 
     def navigation_remaining_duration(self, robot_name: str, cmd_id: int):
@@ -246,24 +322,56 @@ class RobotAPI:
     def stop(self, robot_name: str, cmd_id: int):
         ''' Command the robot to stop.
             Return True if robot has successfully stopped. Else False'''
-        cancel_all_goals = {"id":""}
-        self.client.publish("cancel/"+robot_name ,json.dumps(cancel_all_goals), 2)
+        cancel_all_goals = {"id": ""}
+        self.client.publish(
+            "cancel/" + robot_name,
+            json.dumps(cancel_all_goals),
+            2
+        )
         return True
 
     def navigation_completed(self, robot_name: str, cmd_id: int):
         ''' Return True if the robot has successfully completed its previous
             navigation request. Else False.'''
-        distance = (math.sqrt((self.x_goal[robot_name]-self.x[robot_name])**2 + (self.y_goal[robot_name]-self.y[robot_name])**2))
+        distance = (
+            math.sqrt(
+                (
+                    self.x_goal[robot_name] - self.x[robot_name]
+                )**2 + (
+                    self.y_goal[robot_name] - self.y[robot_name]
+                )**2
+            )
+        )
         print("Theta " + str(self.theta[robot_name]), flush=True)
-        print("Theta goal " +  str(self.theta_goal[robot_name]), flush=True)
-        print("Dif theta " + str(abs(self.theta[robot_name]-self.theta_goal[robot_name])), flush=True)
-        #if (self.resultgoal.get(robot_name) is not None and self.resultgoal[robot_name] == 3):
-        if ((distance < self.goal_dist) and (abs(abs(self.theta[robot_name])-abs(self.theta_goal[robot_name]))) <= 0.05):
+        print("Theta goal " + str(self.theta_goal[robot_name]), flush=True)
+        print(
+            "Dif theta " + str(
+                abs(
+                    self.theta[robot_name] - self.theta_goal[robot_name]
+                )
+            ),
+            flush=True
+        )
+        # if (self.resultgoal.get(robot_name) \
+        # is not None and self.resultgoal[robot_name] == 3):
+        if (
+            (
+                distance < self.goal_dist
+            ) and (
+                abs(
+                    abs(
+                        self.theta[robot_name]
+                    ) - abs(
+                        self.theta_goal[robot_name]
+                    )
+                )
+            ) <= 0.05
+        ):
             self.resultgoal[robot_name] = 0
-            print("Navigation completed! " +  robot_name, flush=True)
+            print("Navigation completed! " + robot_name, flush=True)
             return True
         else:
-            #print("Navigation not completed " + robot_name + " " + str(distance), flush=True)
+            # print("Navigation not completed " + robot_name + " " + str(distance), flush=True)
             return False
 
     def process_completed(self, robot_name: str, cmd_id: int):
@@ -275,10 +383,10 @@ class RobotAPI:
         ''' Return the state of charge of the robot as a value between 0.0
             and 1.0. Else return None if any errors are encountered'''
         if self.battery.get(robot_name) is not None:
-           #print("Battery " + str(self.battery.get(robot_name)), flush=True)
-            return self.battery[robot_name]/100
+            # print("Battery " + str(self.battery.get(robot_name)), flush=True)
+            return self.battery[robot_name] / 100
         else:
-            #print("Battery none ", flush=True)
+            # print("Battery none ", flush=True)
             return None
 
     def requires_replan(self, robot_name: str):
@@ -286,25 +394,49 @@ class RobotAPI:
         return False
 
     def pub_dispenser_requests(self, robot_name: str, task_id: str):
-        data = { "data": task_id }
-        self.client.publish(robot_name + self.dispenser_topic ,json.dumps(data), 2)
+        data = {"data": task_id}
+        self.client.publish(
+            robot_name + self.dispenser_topic,
+            json.dumps(data),
+            2
+        )
 
     def pub_ingestor_requests(self, robot_name: str, task_id: str):
-        data = { "data": task_id }
-        self.client.publish(self.ingestor_topic + robot_name ,json.dumps(data), 2)
+        data = {"data": task_id}
+        self.client.publish(
+            self.ingestor_topic + robot_name,
+            json.dumps(data),
+            2
+        )
 
     def publish_action_execution(self, robot_name: str):
-        data = { "data": True }
-        self.client.publish(self.start_ae_topic + robot_name ,json.dumps(data), 2)
+        data = {"data": True}
+        self.client.publish(
+            self.start_ae_topic + robot_name,
+            json.dumps(data),
+            2
+        )
 
     def publish_door_request(self, door_name: str, requested_mode: str):
-        data = { "text": [door_name, requested_mode]}
-        self.client.publish(self.door_request_topic,json.dumps(data), 2)
+        data = {"text": [door_name, requested_mode]}
+        self.client.publish(
+            self.door_request_topic,
+            json.dumps(data),
+            2
+        )
 
     def publish_dock_request(self, robot_name: str, data: bool):
-        data = { "data": data }
-        self.client.publish(self.dock_request_topic + robot_name ,json.dumps(data), 2)
+        data = {"data": data}
+        self.client.publish(
+            self.dock_request_topic + robot_name,
+            json.dumps(data),
+            2
+        )
 
     def publish_undock_request(self, robot_name: str, data: bool):
-        data = { "data": data }
-        self.client.publish(self.undock_request_topic + robot_name ,json.dumps(data), 2)
+        data = {"data": data}
+        self.client.publish(
+            self.undock_request_topic + robot_name,
+            json.dumps(data),
+            2
+        )
